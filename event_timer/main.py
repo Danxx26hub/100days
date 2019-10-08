e32f34a2edbe230a7481092715d6a8b61658e7b0
Daniel Galvan <danxx26@gmail.com>
2019-10-07 19:02:52 -0700

    added a header function

--- event_timer/main.py
+++ event_timer/main.py
@@ -3,11 +3,21 @@ from datetime import datetime
 __author__ = 'danxx26@gmail'
 
 def main():
+    print_header()
+
     today = datetime.today().day
     month = int(input('please enter month [MM] '))
     day = int(input('please enter a date [DD]'))
     
     print(f'your event is {event_time(today, month, day)} from today')
+
+
+def print_header():
+    print('-' * 30)
+    print('   Event Timer          ')
+    print('   enter your Month / Date ')
+    print('-' * 30)
+    print()
 
 def event_time(today, month, day):
     today = datetime.today()
