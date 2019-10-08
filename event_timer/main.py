222e6dc5c84cba1d27c51eed9d5d45eb63fafc36
Daniel Galvan <danxx26@gmail.com>
2019-10-07 18:48:00 -0700

    added actual code for event timer

--- event_timer/main.py
+++ event_timer/main.py
@@ -3,10 +3,20 @@ from datetime import datetime
 __author__ = 'danxx26@gmail'
 
 def main():
-    pass
+    today = datetime.today().day
+    month = int(input('please enter month [MM] '))
+    day = int(input('please enter a date [DD]'))
+    
+    print(f'your event is {event_time(today, month, day)} from today')
 
-def event_time(date):
-    pass
+def event_time(today, month, day):
+    today = datetime.today()
+    event = datetime(2019, month, day)
+    event_date = (event - today)
+
+    return event_date
+
+    
 
 
 if __name__ == '__main__':
