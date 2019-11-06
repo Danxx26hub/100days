613c3c0600eb3d76b99e725d4f8eb7448de745ac
Daniel Galvan <danxx26@gmail.com>
2019-11-05 22:44:17 -0800

    testing Mikrotik api with librouteros

---
+++ mikrotik/main.py
@@ -0,0 +1,66 @@
+import ssl
+import sys
+from librouteros import connect
+
+
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+con = connect(
+    username='test',
+    password='New@yellow22',
+    host='192.168.88.1',
+    ssl_wrapper=ctx.wrap_socket,
+    port=8729
+    )
+
+s = True
+while s:
+    print('select options:\n')
+
+    choice = input("""select H for health status, L for log, P for system profile, X to exit:\n""".center(50))
+    print()
+    choice = choice.lower()
+    if choice == "h":
+        print("getting health info")
+        health = con(cmd="/system/health/print")
+        for stats in health:
+            for voltage, temp in stats.items():
+                voltage = stats.get('voltage')
+                temp = stats.get('temperature')
+                if temp > 47:
+                    print("too hot shutting down router!!")
+
+                else:
+                    print(f"voltage is {voltage}")
+                    print(f"temp is fine .... {temp} degrees")
+                    #con(cmd="/system/reboot")
+    elif choice == "l":
+        print("reading logs")
+        logs = con(cmd='/log/print')
+        for log in logs:
+            topics = log.get('topics')
+            time = log.get('time')
+            message = log.get('message')
+            if topics != 'firewall,info':
+                print(f"{topics} : {time} : {message}")
+            else:            
+                continue
+    elif choice == 'p':
+        print('running CPU / Memory profile\n')
+        print('please wait.......')
+        
+        profiles = con(cmd="/tool/profile", duration='10', cpu='all')
+        for profile in profiles:
+            device = profile.get('name')
+            usage = profile.get('usage')
+            print(f'{device} : {usage}')
+        print("Done!")
+        print()
+    else:
+        print('exiting program')
+        s = False
+
+
+
