987a2262c9eda0472ded7b2cc38c9953d6f5b373
Daniel Galvan <danxx26@gmail.com>
2019-12-30 23:21:47 -0600

    added timing capabilites, import time module

--- timer/main.py
+++ timer/main.py
@@ -1,10 +1,15 @@
+import time
 from reader import feed
 
 def main():
     """download the latest tutorial from RP"""
+    tic = time.perf_counter() # start counter 
 
 
     tutorial = feed.get_article(0)
+    toc = time.perf_counter() # stop timing
+
+    print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
     print(tutorial)
 
 
