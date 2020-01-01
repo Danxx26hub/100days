c1240cdac18090013f3417dc694da3b03f08db44
Daniel Galvan <danxx26@gmail.com>
2020-01-01 02:24:56 -0600

    added timer class and imported

--- timer/main.py
+++ timer/main.py
@@ -1,16 +1,23 @@
-import time
 from reader import feed
+from timer import Timer
+
+
+t = Timer()
 
 def main():
     """download the latest tutorial from RP"""
-    tic = time.perf_counter() # start counter 
+    #tic = time.perf_counter() # start counter
+    t.start() 
 
 
     tutorial = feed.get_article(0)
-    toc = time.perf_counter() # stop timing
+    #toc = time.perf_counter() # stop timing
+    t.stop()
 
-    print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
-    print(tutorial)
+    
+    print()
+    print()
+    #print(tutorial)
 
 
 if __name__=='__main__':
